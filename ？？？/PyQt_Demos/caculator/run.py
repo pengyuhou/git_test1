15234530fdcba84db72c861c10a3dad4001b0745
786490473 <786490473@qq.com>
2020-04-30 23:47:46 +0800

    python实现计算器

---
+++ ？？？/PyQt_Demos/caculator/run.py
@@ -0,0 +1,15 @@
+import sys
+import c1
+from PyQt5 import QtWidgets
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainwindow = QtWidgets.QMainWindow()
+    ui = c1.Ui_MainWindow()
+    ui.setupUi(mainwindow)
+    mainwindow.show()
+    sys.exit(app.exec_())
+
+
+
+
