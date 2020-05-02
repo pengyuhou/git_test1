01c5d23ba6065f9ca52a5ece2f80d03fd879fa2d
786490473 <786490473@qq.com>
2020-05-02 10:11:20 +0800

    leetcode打卡

---
+++ leetcode/3. 无重复字符的最长子串.py
@@ -0,0 +1,42 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        s = list(s)
+        ret = []
+        l = 0
+        for i in s:
+            if i not in ret:
+                ret.append(i)
+            else:
+                ret.append(i)
+                ret = ret[ret.index(i)+1:]
+            l = max(l, len(ret))
+        return l
+
+if __name__ == "__main__":
+
+    print(Solution().lengthOfLongestSubstring('abcabcbb'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
