6cd67698532dcf754574f5157504d43081fdc3e1
786490473 <786490473@qq.com>
2020-05-07 11:49:24 +0800

    继续测试

---
+++ ？？？/dmeo.py
@@ -0,0 +1,13 @@
+import math
+
+x = ((50 ** 2 + 110 ** 2 - 2 * 50 * 110 * math.cos(math.pi / 6)) ** 0.5)
+
+# print(math.sin(math.pi/6))
+print(x * math.sin(2 * math.pi * 70 / 360) / math.sin(2 * math.pi * 40 / 360))
+print(110 * 0.5 / 71.23)
+
+print(all([True, True, False]))
+
+a = '   afasdfa   afdasd   '
+ret = a.split()
+print(ret)
